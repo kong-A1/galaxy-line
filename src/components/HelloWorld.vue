a4ed67a546de078d40b6147094111ffc7ec54c5b
kong-A1 <kong@relationshiprepublic.com>
2026-07-16 18:51:22 +0700

    test ci failed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,7 +18,7 @@ const count = ref(0)
       <h1>Get started</h1>
       <p>Edit <code>src/App.vue</code> and save to test <code>HMR</code></p>
     </div>
-    <button type="button" class="counter" @click="count++">
+    <button type="button" class="counter" @click="count++"
       Count is {{ count }}
     </button>
   </section>
